65bf81621868bbcd65b108736d6bab887912d47a
Linda Foo <linda.foo.dev@gmail.com>
2018-02-07 13:22:31 -0800

    added hamburger button toggle show/hide nav options for mobile

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ document.querySelector("li.howitworks").addEventListener("click", function () {
   }
   console.log('clicked! 2');
 });
+
+
+document.querySelector("div.hamburger").addEventListener("click", function () {
+  document.querySelector("li.courses").classList.toggle('noDisplay');
+  console.log('clicked!');  
+
+  if (!document.querySelector("nav.courses").classList.contains('noDisplay')) {
+    document.querySelector("nav.courses").classList.toggle('noDisplay');
+  }
+  
+  document.querySelector("li.howitworks").classList.toggle('noDisplay');
+  console.log('clicked!');  
+
+  if (!document.querySelector("nav.howitworks").classList.contains('noDisplay')) {
+    document.querySelector("nav.howitworks").classList.toggle('noDisplay');
+  }
+  
+  document.querySelector("li.reviews").classList.toggle('noDisplay');
+  console.log('clicked!');
+
+  document.querySelector("li.pricing").classList.toggle('noDisplay');
+  console.log('clicked!');
+
+  document.querySelector("li.signin").classList.toggle('noDisplay');
+  console.log('clicked!');
+
+});
