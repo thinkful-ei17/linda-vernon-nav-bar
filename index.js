c9ee274b8d36ad3fff5abf34f861b60777fbb625
Vernon Mensah <vernonmensah@gmail.com>
2018-02-07 15:59:17 -0500

    Fixed mobile view

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 document.querySelector("li.courses").addEventListener("click", function () {
-  document.querySelector("div.courses").classList.toggle('noDisplay');
+  document.querySelector("nav.courses").classList.toggle('noDisplay');
   console.log('clicked!');
 
-  if (!document.querySelector("div.howitworks").classList.contains('noDisplay')) {
-    document.querySelector("div.howitworks").classList.toggle('noDisplay');
+  if (!document.querySelector("nav.howitworks").classList.contains('noDisplay')) {
+    document.querySelector("nav.howitworks").classList.toggle('noDisplay');
   }
 
 });
 
 document.querySelector("li.howitworks").addEventListener("click", function () {
-  document.querySelector("div.howitworks").classList.toggle('noDisplay');
+  document.querySelector("nav.howitworks").classList.toggle('noDisplay');
 
-  if (!document.querySelector("div.courses").classList.contains('noDisplay')) {
-    document.querySelector("div.courses").classList.toggle('noDisplay');
+  if (!document.querySelector("nav.courses").classList.contains('noDisplay')) {
+    document.querySelector("nav.courses").classList.toggle('noDisplay');
   }
   console.log('clicked! 2');
 });
